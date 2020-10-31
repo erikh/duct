b5d0fbbf8d55756f6b2bae0a48f3c2e3aa3d8c02
Erik Hollensbe <github@hollensbe.org>
2020-10-30 19:20:53 -0700

    some tests for early terminating containers
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- duct_test.go
+++ duct_test.go
@@ -3,6 +3,7 @@ package duct
 import (
 	"context"
 	"testing"
+	"time"
 )
 
 func TestBasic(t *testing.T) {
@@ -16,6 +17,46 @@ func TestBasic(t *testing.T) {
 			},
 			BindMounts: map[string]string{
 				"duct.go": "/duct.go",
+			},
+		},
+	})
+
+	if err := c.Launch(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := c.Teardown(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+
+	c = New(Manifest{
+		"early-terminator": {
+			Command:  []string{"sleep", "1"},
+			Image:    "alpine:latest",
+			BootWait: 2 * time.Second,
+			PostCommands: [][]string{
+				{"echo", "from post-command"},
+			},
+		},
+	})
+
+	if err := c.Launch(context.Background()); err == nil {
+		t.Fatal("launch succeeded; should not have")
+	}
+
+	if err := c.Teardown(context.Background()); err == nil {
+		t.Fatal("teardown did not fail with an error")
+	}
+
+	// start it a second time to make sure it was cleaned up, this time it will
+	// succeed to run
+	c = New(Manifest{
+		"early-terminator": {
+			Command:  []string{"sleep", "3"},
+			Image:    "alpine:latest",
+			BootWait: 2 * time.Second,
+			PostCommands: [][]string{
+				{"echo", "from post-command"},
 			},
 		},
 	})
