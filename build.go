3d3a88502c48a98394d4c1aaf2cb10647f86bced
Erik Hollensbe <github@hollensbe.org>
2020-10-31 23:59:06 -0700

    More build docs
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- build.go
+++ build.go
@@ -8,16 +8,19 @@ import (
 	dc "github.com/fsouza/go-dockerclient"
 )
 
-// Build is a set of instructions for building a container image
+// Build is a set of instructions for building a container image. All paths are
+// relative to the working directory of the test.
 type Build struct {
+	// Dockerfile is the path to the dockerfile.
 	Dockerfile string
-	Context    string
+	// Context is the directory to use.
+	Context string
 }
 
 // Builder is a named collection of builds.
 type Builder map[string]Build
 
-// Run runs the builds.
+// Run runs the builds. It logs them to stderr similarly to `docker build`.
 func (bc Builder) Run(ctx context.Context) error {
 	client, err := dc.NewClientFromEnv()
 	if err != nil {
