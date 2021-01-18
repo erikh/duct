aca80cf91e23270b5452cc2b1f60764b70cd3c0f
Shawn Burke <github@shawnburke.com>
2021-01-18 21:54:32 +0000

    Add ability to redirect logging

--- duct.go
+++ duct.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -80,8 +81,18 @@ type Composer struct {
 // New constructs a new Composer from a Manifest. A network name must also be
 // provided; it will be created and cleaned up when Run and Teardown are
 // called.
-func New(manifest Manifest, options Options) *Composer {
-	return &Composer{manifest: manifest, options: options}
+func New(manifest Manifest, options ...Options) *Composer {
+
+	// Gather options
+	opts := Options{}
+
+	for _, o := range options {
+		for k, v := range o {
+			opts[k] = v
+		}
+	}
+
+	return &Composer{manifest: manifest, options: opts}
 }
 
 // Options is a generic type for options.
@@ -90,6 +101,7 @@ type Options map[string]interface{}
 const (
 	optionCreateNetwork   = "create_network"
 	optionExistingNetwork = "existing_network"
+	optionLogWriter       = "log_writer"
 )
 
 // WithNewNetwork creates a network for use with the manifest.
@@ -101,6 +113,12 @@ func WithNewNetwork(name string) Options {
 // network names are not unique!)
 func WithExistingNetwork(id string) Options {
 	return Options{optionExistingNetwork: id}
+}
+
+// WithLogWriter routes all logging output to the specified writer, or to none
+// if nil is pecified
+func WithLogWriter(writer io.Writer) Options {
+	return Options{optionLogWriter: writer}
 }
 
 // HandleSignals handles SIGINT and SIGTERM to ensure that containers get
@@ -138,12 +156,26 @@ func (c *Composer) GetNetworkID() string {
 	return c.netID
 }
 
+type nullWriter struct{}
+
+func (nw nullWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
 // Launch launches the manifest. On error containers are automatically cleaned
 // up.
 func (c *Composer) Launch(ctx context.Context) error {
 	client, err := dc.NewClientFromEnv()
 	if err != nil {
 		return err
+	}
+
+	if w, ok := c.options[optionLogWriter]; ok {
+		var writer io.Writer = w.(io.Writer)
+		if writer == nil {
+			writer = nullWriter{}
+		}
+		log.SetOutput(writer)
 	}
 
 	if c.options[optionCreateNetwork] != nil {
@@ -324,7 +356,7 @@ func (c *Composer) Teardown(ctx context.Context) error {
 
 			log.Printf("Removing container: [%s]", cont.Name)
 			if err := client.RemoveContainer(dc.RemoveContainerOptions{ID: cont.id, Force: true, Context: ctx}); err != nil {
-				log.Println(err)
+				log.Printf("Error shutting down continer: [%s] %v\n", cont.Name, err)
 				errs = true
 			}
 		} else {
