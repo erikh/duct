d4e5bf1ea906693468bd945d8fa8a448d83c5dbe
Erik Hollensbe <git@hollensbe.org>
2024-01-22 16:29:10 -0800

    fix test harness for architectures and new download url
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- test.sh
+++ test.sh
@@ -2,6 +2,17 @@
 
 nohup dockerd -s vfs &
 
-curl -sSL https://storage.googleapis.com/golang/go${GOLANG_VERSION:-1.20.1}.linux-amd64.tar.gz | tar -xz -C /usr/local
+machine="amd64"
+
+case "$(uname -m)" in
+  aarch64)
+    machine="arm64"
+    ;;
+esac
+  
+
+url=https://go.dev/dl/go${GOLANG_VERSION:-1.21.6}.linux-${machine}.tar.gz 
+echo "Fetching golang from $url"
+curl -sSL "$url" | tar -xz -C /usr/local
 
 PATH="${PATH}:/usr/local/go/bin" go test -v ./... -count 1
